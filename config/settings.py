8892176c361d17915ff6714d3d834275ca1da301
shipping <master@shipping.or.kr>
2022-02-16 20:15:26 +0900

    RDS update

--- config/settings.py
+++ config/settings.py
@@ -186,7 +186,6 @@ else:
         "default": {
             "ENGINE": "django.db.backends.postgresql",
             "HOST": os.environ.get("RDS_HOST"),
-            "NAME": os.environ.get("RDS_NAME"),
             "USER": os.environ.get("RDS_USER"),
             "PASSWORD": os.environ.get("RDS_PASSWORD"),
             "PORT": os.environ.get("RDS_PORT"),
