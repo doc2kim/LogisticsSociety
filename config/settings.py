d6c97a834487b4d5dc5ce8795ffc5a9fdfb6d85e
shipping <master@shipping.or.kr>
2022-02-17 11:33:27 +0900

    static

--- config/settings.py
+++ config/settings.py
@@ -151,9 +151,9 @@ TEMPLATES = [
         },
     },
 ]
-#os.path.join((BASE_DIR, "frontend"),"build", "static"),
+#
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+   os.path.join((BASE_DIR, "frontend"),"build", "static"),
 ]
 
 
