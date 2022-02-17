ba7d51c7a0408237710745e08417ce935db784b6
shipping <master@shipping.or.kr>
2022-02-17 16:04:55 +0900

    static url update

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name='frontend/build/index.html')),
+    path("", TemplateView.as_view(template_name='index.html')),
     # 'api/'path에 users.urls의 path를 포함시켜 users.urls의 path에 'api/'가 앞에 붙음
     path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
