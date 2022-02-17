1676fd42f4fb1fc537273277c48aceb548709afc
shipping <master@shipping.or.kr>
2022-02-17 16:06:40 +0900

    static url update

--- config/urls.py
+++ config/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name='index.html')),
     # 'api/'path에 users.urls의 path를 포함시켜 users.urls의 path에 'api/'가 앞에 붙음
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
