067a47f02f97dc16571b2477423235bbdf30d337
shipping <master@shipping.or.kr>
2022-02-18 20:13:21 +0900

    sadf

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = [
 # BASE_DIR = Path(__file__).ancestor(2)
 
 # 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 WSGI_APPLICATION = 'config.wsgi.application'
