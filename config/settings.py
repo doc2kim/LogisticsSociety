9970a8fe6741f949194ee0e9aa55455712479e4d
shipping <master@shipping.or.kr>
2022-02-19 15:03:17 +0900

    aaa

--- config/settings.py
+++ config/settings.py
@@ -145,42 +145,44 @@ TEMPLATES = [
 ]
 
 
+
+# DEBUG = True
 DEBUG = bool(os.environ.get("DEBUG"))
 
 #
+STATICFILES_DIRS = [
+        os.path.join(REACT_APP_DIR,"build"),
+        ]
 
-# BASE_DIR = Path(__file__).ancestor(2)
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    
+STATIC_URL = "/static/"
+WSGI_APPLICATION = 'config.wsgi.application'
 # 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-if not DEBUG:
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    WSGI_APPLICATION = 'config.wsgi.application'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# if not DEBUG:
+#     # DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+#     # 
+#     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+#     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+#     AWS_STORAGE_BUCKET_NAME = "shipping-s3"
+#     AWS_REGION = "ap-northeast-2"
+#     AWS_LOCATION = "static"
 
-
-    # DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
-    # 
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "shipping-s3"
-    AWS_REGION = "ap-northeast-2"
-    AWS_LOCATION = "static"
-
-    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max_age=86400"}
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
-    AWS_DEFAULT_ACL = "public-read"
-    AWS_S_FILE_OVERWRITE = False
-    #안되면 https -> http
-    STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max_age=86400"}
+#     AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+#     AWS_DEFAULT_ACL = "public-read"
+#     AWS_S_FILE_OVERWRITE = False
+#     #안되면 https -> http
+#     STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
-if DEBUG:
+# if DEBUG:
+#     MEDIA_URL = '/media/'
+#     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     
-    STATICFILES_DIRS = [
-        os.path.join(REACT_APP_DIR,"build","static"),
-    ]
-    STATIC_URL = "/static"
+#     STATIC_URL = "/static/"
 
 
 
@@ -190,7 +192,6 @@ if DEBUG:
 
 
 
-# DEBUG = True
 
 
 if DEBUG:
