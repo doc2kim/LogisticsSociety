cb243ee657c2a1c69f076082c172cb32b17cd93a
shipping <master@shipping.or.kr>
2022-02-21 10:05:20 +0900

    static

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ DEBUG = bool(os.environ.get("DEBUG"))
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
 
 STATICFILES_DIRS = [
-        os.path.join(REACT_APP_DIR,"build", "static"),
+        os.path.join(REACT_APP_DIR,"build"),
         ]
 
 MEDIA_URL = '/media/'
