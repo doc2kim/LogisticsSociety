5608f1893e2b26dfa56c8f4b9d3106d0d3fc55c2
shipping <master@shipping.or.kr>
2022-02-21 10:09:35 +0900

    asdf

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include, re_path
+from django.urls import path, include
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 
@@ -11,5 +12,4 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name='index.html')),
     # 'api/'path에 users.urls의 path를 포함시켜 users.urls의 path에 'api/'가 앞에 붙음
     path('api/', include('api.urls')),
-] 
-# +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
