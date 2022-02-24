3e3676e524b2637766e5b51564be9648d844a3c4
shipping <master@shipping.or.kr>
2022-02-24 16:29:23 +0900

    aadd

--- config/urls.py
+++ config/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name='index.html')),
     # 'api/'path에 users.urls의 path를 포함시켜 users.urls의 path에 'api/'가 앞에 붙음
     path('api/', include('api.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
