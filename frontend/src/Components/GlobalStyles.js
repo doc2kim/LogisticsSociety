30521c97d084af0f8965832f51c8a2cca03fd3fa
shipping <master@shipping.or.kr>
2022-02-28 09:53:46 +0900

    aa

--- frontend/src/Components/GlobalStyles.js
+++ frontend/src/Components/GlobalStyles.js
@@ -10,29 +10,12 @@ const globalStyle = createGlobalStyle`
     *{
         box-sizing:content-box;
         font-size: 1em;
-        /* @font-face{
-            font-family: 'Noto Sans KR', sans-serif;
-            font-weight: 100 ~ 900;
-        } */
-        
-    }
-    h1,h2,h3,h4,span,a,div,ul,li,input,form{
-        /* @font-face{
-            font-family: 'Noto Sans KR', sans-serif;
-            font-weight: 100 ~ 900;
-        } */
     }
     
     html,
     body{
         width:100%;
-        font-family: 'Nanum Pen Script', cursive;
-        /* font-family: 'Apple SD Gothic Neo','Malgun Gothic',arial,sans-serif; */
-        /* @font-face{
-            font-family: 'Noto Sans KR', sans-serif;
-            font-weight: 100 ~ 900;
-        } */
-
+        font-family: 'Noto Sans KR', sans-serif;
         @media only screen and (max-width:768px) {
             overflow-x: hidden;
         }
