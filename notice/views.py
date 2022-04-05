3bf28f31bfa4c4be14e04e2659e469ad965ca981
shipping <master@shipping.or.kr>
2022-04-05 13:19:47 +0900

    aa

--- notice/views.py
+++ notice/views.py
@@ -1,10 +1,27 @@
 from .models import Notice
 from .serializers import NoticeSerializer
 from rest_framework import viewsets
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, Http404
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.filters import SearchFilter
+# import urllib
+# import os
+# import mimetypes
+
+# def notice_download_view(request, pk):
+#     notice = get_object_or_404(Notice, pk=pk)
+#     url = notice.upload_files.url[1:]
+#     file_url = urllib.parse.unquote(url)
+    
+#     if os.path.exists(file_url):
+#         with open(file_url, 'rb') as fh:
+#             quote_file_url = urllib.parse.quote(notice.filename.encode('utf-8'))
+#             response = HttpResponse(fh.read(), content_type=mimetypes.guess_type(file_url)[0])
+#             response['Content-Disposition'] = 'attachment;filename*=UTF-8\'\'%s' % quote_file_url
+#             return response
+#     raise Http404
+
 
 
 class NoticeView(viewsets.ModelViewSet):
