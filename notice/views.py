272684e41e5b36619d0de5a18c4613f587297455
shipping <master@shipping.or.kr>
2022-04-07 10:04:42 +0900

    convmv

--- notice/views.py
+++ notice/views.py
@@ -10,18 +10,18 @@ from rest_framework.filters import SearchFilter
 # import os
 # import mimetypes
 
-# def notice_download_view(request, pk):
-#     notice = get_object_or_404(Notice, pk=pk)
-#     url = notice.upload_files.url[1:]
-#     file_url = urllib.parse.unquote(url)
+def notice_download_view(request, pk):
+    notice = get_object_or_404(Notice, pk=pk)
+    url = notice.upload_files.url[1:]
+    file_url = urllib.parse.unquote(url)
     
-#     if os.path.exists(file_url):
-#         with open(file_url, 'rb') as fh:
-#             quote_file_url = urllib.parse.quote(notice.filename.encode('utf-8'))
-#             response = HttpResponse(fh.read(), content_type=mimetypes.guess_type(file_url)[0])
-#             response['Content-Disposition'] = 'attachment;filename*=UTF-8\'\'%s' % quote_file_url
-#             return response
-#     raise Http404
+    if os.path.exists(file_url):
+        with open(file_url, 'rb') as fh:
+            quote_file_url = urllib.parse.quote(notice.filename.encode('utf-8'))
+            response = HttpResponse(fh.read(), content_type=mimetypes.guess_type(file_url)[0])
+            response['Content-Disposition'] = 'attachment;filename*=UTF-8\'\'%s' % quote_file_url
+            return response
+    raise Http404
 
 class NoticeView(viewsets.ModelViewSet):
     serializer_class = NoticeSerializer
